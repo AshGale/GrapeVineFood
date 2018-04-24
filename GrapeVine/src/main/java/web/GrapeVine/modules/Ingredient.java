e7fd0ba66b532e2d4d760ae409a79a21c43a626a
AshGale <ashgale17@yahoo.co.uk>
2018-04-24 23:48:20 +0100

    updated fields for ingredient added getters setters, constructors and tostring methods

--- GrapeVine/src/main/java/web/GrapeVine/modules/Ingredient.java
+++ GrapeVine/src/main/java/web/GrapeVine/modules/Ingredient.java
@@ -1,5 +1,7 @@
 package web.GrapeVine.modules;
 
+import java.sql.Blob;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 //@Entity
@@ -9,8 +11,26 @@ public class Ingredient {
 	//@GeneratedValue(strategy=GenerationType.AUTO)
 	Long id;
 	String name;
-	int grams;
+	String allergy;//TODO update object
+	int singleWeight;
+	String nutritionalValue;//TODO make object Json fields of vitamins or foods
+	String detailedLink;
+	Blob image;
 	
+	public Ingredient() {
+		super();		
+	}
+	public Ingredient(Long id, String name, String allergy, int singleWeight, String nutritionalValue,
+			String detailedLink, Blob image) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.allergy = allergy;
+		this.singleWeight = singleWeight;
+		this.nutritionalValue = nutritionalValue;
+		this.detailedLink = detailedLink;
+		this.image = image;
+	}
 	public Long getId() {
 		return id;
 	}
@@ -23,16 +43,43 @@ public class Ingredient {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public int getGrams() {
-		return grams;
+	public String getAllergy() {
+		return allergy;
 	}
-	public void setGrams(int grams) {
-		this.grams = grams;
+	public void setAllergy(String allergy) {
+		this.allergy = allergy;
 	}
-	
+	public int getSingleWeight() {
+		return singleWeight;
+	}
+	public void setSingleWeight(int singleWeight) {
+		this.singleWeight = singleWeight;
+	}
+	public String getNutritionalValue() {
+		return nutritionalValue;
+	}
+	public void setNutritionalValue(String nutritionalValue) {
+		this.nutritionalValue = nutritionalValue;
+	}
+	public String getDetailedLink() {
+		return detailedLink;
+	}
+	public void setDetailedLink(String detailedLink) {
+		this.detailedLink = detailedLink;
+	}
+	public Blob getImage() {
+		return image;
+	}
+	public void setImage(Blob image) {
+		this.image = image;
+	}
 	@Override
 	public String toString() {
-		return "Ingredient [id=" + id + ", name=" + name + ", grams=" + grams + "]";
-	}	
+		return "Ingredient [id=" + id + ", name=" + name + ", allergy=" + allergy + ", singleWeight=" + singleWeight
+				+ ", nutritionalValue=" + nutritionalValue + ", detailedLink=" + detailedLink + ", image=" + image
+				+ "]";
+	}
+	
+	
 	
 }
--- GrapeVine/src/main/java/web/GrapeVine/modules/Profile.java
+++ GrapeVine/src/main/java/web/GrapeVine/modules/Profile.java
@@ -18,47 +18,128 @@ public class Profile {
 	String userName;
 	String email;
 	String password;//TODO add encryption and privacy
+	//Eating Habits
 	String allergys;//TODO make object booleans
 	String preferances;//TODO make object booleans
 	String dislikes;//TODO make object Dynamic list or eg 5 items
 	String diet;//TODO make object of set of dislikes
 	String defaultLocation; //TODO on maps determine coordinates for optimisation
 	
-	
-	
 	public Profile() {
 		super();
 	}
 
-	public Profile(Long id, String name, String role) {
+	public Profile(Long id, String role, String name, String userName, String email, String password, String allergys,
+			String preferances, String dislikes, String diet, String defaultLocation) {
 		super();
 		this.id = id;
-		this.name = name;
 		this.role = role;
+		this.name = name;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+		this.allergys = allergys;
+		this.preferances = preferances;
+		this.dislikes = dislikes;
+		this.diet = diet;
+		this.defaultLocation = defaultLocation;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
-	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAllergys() {
+		return allergys;
+	}
+
+	public void setAllergys(String allergys) {
+		this.allergys = allergys;
+	}
+
+	public String getPreferances() {
+		return preferances;
+	}
+
+	public void setPreferances(String preferances) {
+		this.preferances = preferances;
+	}
+
+	public String getDislikes() {
+		return dislikes;
+	}
+
+	public void setDislikes(String dislikes) {
+		this.dislikes = dislikes;
+	}
+
+	public String getDiet() {
+		return diet;
+	}
+
+	public void setDiet(String diet) {
+		this.diet = diet;
+	}
+
+	public String getDefaultLocation() {
+		return defaultLocation;
+	}
+
+	public void setDefaultLocation(String defaultLocation) {
+		this.defaultLocation = defaultLocation;
+	}
+
 	@Override
 	public String toString() {
-		return "Member [id=" + id + ", name=" + name + ", role=" + role + "]";
+		return "Profile [id=" + id + ", role=" + role + ", name=" + name + ", userName=" + userName + ", email=" + email
+				+ ", password=" + password + ", allergys=" + allergys + ", preferances=" + preferances + ", dislikes="
+				+ dislikes + ", diet=" + diet + ", defaultLocation=" + defaultLocation + "]";
 	}
+	
+
 	
 }
